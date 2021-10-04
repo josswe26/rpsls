838d1e8c4fa18de6ba31d4e896f51021db2ba760
josswe26 <josec.sanchez@icloud.com>
2021-10-04 20:31:24 +0000

    Add code structure to script.js  and change id names for game area

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+//Game code
+
+function displayWeapon() {
+
+}
+
+function computerWeapon() {
+
+}
+
+function compareWeapons() {
+
+}
+
+function increaseUserScore() {
+
+}
+
+function increaseComputerScore() {
+
+}
+
+function countRounds() {
+
+}
+
+//Popups code
+
+function openPopup() {
+
+}
+
+function closePopup() {
+
+}
+
+
+
