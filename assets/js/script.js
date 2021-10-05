a75fa0707ddec2e4cdac21bf178052e3fbf63e5a
josswe26 <josec.sanchez@icloud.com>
2021-10-05 18:30:27 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
+// Wait for the DOM to finish loading before running the code
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    //Add event listener to game buttons
+    let buttons = document.getElementsByClassName("game-btn");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let userChoice = this.getAttribute("data-type");
+            let userIcon = document.getElementById("user-icon");
+
+            console.log(userChoice, userIcon);
+        })
+    }
+
+    //Add event listener to buttons opening the popups
+    let openPopupButtons = document.getElementsByClassName("open-popup-btn");
+
+    for (let button of openPopupButtons) {
+        button.addEventListener("click", function() {
+            let buttonSelected = this.getAttribute("id");
+            
+            console.log(buttonSelected);
+        })
+    }
+
+    //Add event listener to close buttons on the popups
+    let closePopupButtons = document.getElementsByClassName("close-popup-btn");
+
+    for (let button of closePopupButtons) {
+        button.addEventListener("click", function() {
+            let buttonSelected = this.getAttribute("id");
+            
+            console.log(buttonSelected);
+        })
+    }
+
+    //Add event listener to the start game button
+    document.getElementById("start-game-btn").addEventListener("click", function() {
+        
+        console.log("Start button pressed")
+    })
+})
+
 //Game code
 
 function displayWeapon() {
@@ -27,7 +73,7 @@ function countRounds() {
 //Popups code
 
 function openPopup() {
-
+    
 }
 
 function closePopup() {
